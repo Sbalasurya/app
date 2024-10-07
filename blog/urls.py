eec02c5693b80012ff0711292d8e5e934dd8bd7c
Sbalasurya <suryameena76@gmail.com>
2024-10-07 20:54:19 +0530

    changes

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns=[
     path("", views.index, name="index" ),
-    path("post/detail", views.detail, name ="detail")
+    path("post/<str:post_id>", views.detail, name ="detail"),
+    path("old_url", views.old_url, name="old_url" ),
+    path("new_changed_url", views.new_url, name="new_page_url" ),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello from index page")
+    #return HttpResponse("Hello from index page")
+    return render(request,"index.html")
 
-def detail(request):
-    return HttpResponse("You are viewing details page")
+def detail(request, post_id):
+    return HttpResponse(f"You are viewing details page and id is {post_id}")
+
+def old_url(request):
+    return redirect(reverse('blog:new_page_url'))
+
+def new_url(request):
+    return HttpResponse("This is the new url")

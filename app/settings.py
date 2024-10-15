4dcf20b2a11d91e12e21f963f574f8b1dc0cee62
Sbalasurya <suryameena76@gmail.com>
2024-10-15 11:58:54 +0530

    new views created

--- app/settings.py
+++ app/settings.py
@@ -112,6 +112,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIR = [
+    "/blog/static/blog/"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- blog/views.py
+++ blog/views.py
@@ -5,10 +5,11 @@ from django.urls import reverse
 # Create your views here.
 def index(request):
     #return HttpResponse("Hello from index page")
-    return render(request,"index.html")
+    return render(request,"blog\index.html")
 
 def detail(request, post_id):
-    return HttpResponse(f"You are viewing details page and id is {post_id}")
+    # return HttpResponse("You are viewing details page and id is {post_id}")
+    return render(request,"blog\detail.html")
 
 def old_url(request):
     return redirect(reverse('blog:new_page_url'))
